29a5997b93cc4b1c54214b457f8dc4b459e2cab4
ivan.todorovic@octopus.cloud <ivan.todorovic@octopus.cloud>
2023-06-07 10:16:31 +0200

    FamilyBudget: front-end interceptor for authentication

--- FamilyBudget.Appp/src/app/app.module.ts
+++ FamilyBudget.Appp/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,13 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     NoopAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FamilyBudget.Appp/src/app/services/budget.service.ts
+++ FamilyBudget.Appp/src/app/services/budget.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Budget } from '../models/budget';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { FamilyMember } from '../models/family-member';
@@ -12,15 +12,9 @@ import { BudgetShare } from '../models/budget-share';
 })
 export class BudgetService {
   url = 'Budget';
-  header: HttpHeaders;
   
   constructor(private http: HttpClient) {
-    const token = localStorage.getItem('token');
 
-    this.header = new HttpHeaders({
-      'Authorization': 'Bearer ' + token,
-      'Content-Type': 'application/json'
-    });
   }
 
   public doLogin(email: string, password: string): Observable<any> {
@@ -37,29 +31,21 @@ export class BudgetService {
 
   public listBudgets(): Observable<Budget[]>  {
 
-    return this.http.get<Budget[]>(`${environment.apiUrl}/budget`, {
-      headers: this.header
-    });
+    return this.http.get<Budget[]>(`${environment.apiUrl}/budget`);
   }
 
   public listBudgetsShare(): Observable<BudgetShare[]> {
 
-    return this.http.get<BudgetShare[]>(`${environment.apiUrl}/budgetshare`, {
-      headers: this.header
-    });
+    return this.http.get<BudgetShare[]>(`${environment.apiUrl}/budgetshare`);
   }
 
   public listUsers(): Observable<FamilyMember[]> {
 
-    return this.http.get<FamilyMember[]>(`${environment.apiUrl}/authentication/list`, {
-      headers: this.header
-    });
+    return this.http.get<FamilyMember[]>(`${environment.apiUrl}/authentication/list`);
   }
 
   public listTransactions(): Observable<Transaction[]> {
 
-    return this.http.get<Transaction[]>(`${environment.apiUrl}/transaction`, {
-      headers: this.header
-    });
+    return this.http.get<Transaction[]>(`${environment.apiUrl}/transaction`);
   }
 }
